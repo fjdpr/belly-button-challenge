f6225975c1905d441fa394ca17fc13144f777041
fjdpr <fjdpr@hotmail.com>
2025-01-21 15:35:05 -0600

    add 'console.log' to app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,16 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    console.log("Metadata:", data);
 
     // get the metadata field
     let metadata = data.metadata;
+    console.log("Metadata field:", metadata);
 
     // Filter the metadata for the object with the desired sample number
     let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     let result = resultArray[0];
+    console.log("Filtered Metadata Result:", result);
 
     // Use d3 to select the panel with id of `#sample-metadata`
     let PANEL = d3.select("#sample-metadata");
@@ -27,18 +30,25 @@ function buildMetadata(sample) {
 // function to build both charts
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    console.log("Samples Data:", data)
 
     // Get the samples field
     let samples = data.samples;
+    console.log("Samples field:", samples);
 
     // Filter the samples for the object with the desired sample number
     let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     let result = resultArray[0];
+    console.log("Filtered Samples Result:", result);
 
     // Get the otu_ids, otu_labels, and sample_values
     let otu_ids = result.otu_ids;
     let otu_labels = result.otu_labels;
     let sample_values = result.sample_values;
+
+    console.log("OTU IDs:", otu_ids);
+    console.log("OTU Labels:", otu_labels);
+    console.log("Sample Values:", sample_values);
 
     // Build a Bubble Chart
     let bubbleLayout = {
@@ -59,6 +69,8 @@ function buildCharts(sample) {
         colorscale: "Earth"
       }
     }];
+    
+    console.log("Bubble Data:", bubbleData);
 
     // Render the Bubble Chart
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
@@ -74,8 +86,10 @@ function buildCharts(sample) {
       text: otu_labels.slice(0, 10).reverse(),
       type: "bar",
       orientation: "h",
-    }
-    ];
+    }];
+    
+    console.log("Bar Data:", barData);
+    
     let barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       xaxis: { title: "Number of Bacteria" },
@@ -107,6 +121,7 @@ function init() {
 
     // Get the first sample from the list
     let firstSample = names[0];
+    console.log("First Sample:", firstSample);
 
     // Build charts and metadata panel with the first sample
     buildCharts(firstSample);
@@ -116,6 +131,7 @@ function init() {
 
 // Function for event listener
 function optionChanged(newSample) {
+  console.log("New Sample:", newSample);
   // Build charts and metadata panel each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
